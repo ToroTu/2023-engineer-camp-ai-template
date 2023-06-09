4e0a3b95d6d8f68131863af2319877691b530729
ToroTu <tu8247@gmail.com>
2023-06-09 13:24:04 +0800

    test.

--- services/fetch_idiom.js
+++ services/fetch_idiom.js
@@ -48,15 +48,15 @@ const checkIdiom = async (text) => {
 const getIdiom = async (href) => {
   const url = BASE_URL + href;
   const { data: body } = await axios.get(url);
-  // --- 爬蟲
-  const $ = cheerio.load(body);
-  // let description = $('#row_mean td[headers="th_mean"]').text();
-  // description = description.replace(/\<br\>.+\<a.+/, '');
-  const description = $('#row_useExample td[headers="th_useExample"] h4')
-    .filter((_, item) => item.childNodes[0]?.data == '語義說明')
-    .map((_, item) => item.nextSibling)
-    .map((_, item) => item.data);
-  return description;
+  // // --- 爬蟲
+  // const $ = cheerio.load(body);
+  // // let description = $('#row_mean td[headers="th_mean"]').text();
+  // // description = description.replace(/\<br\>.+\<a.+/, '');
+  // const description = $('#row_useExample td[headers="th_useExample"] h4')
+  //   .filter((_, item) => item.childNodes[0]?.data == '語義說明')
+  //   .map((_, item) => item.nextSibling)
+  //   .map((_, item) => item.data);
+  // return description;
 };
 
 module.exports = { checkIdiom };
