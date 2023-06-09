e48a4bcf1e1f649e8a0e4a5be012d4f25a26df4b
ToroTu <tu8247@gmail.com>
2023-06-09 13:11:30 +0800

    test.

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ const chatAI = require('../services/openai');
 
 const convertToSimp = require('../services/transform_trad2simp');
 
-const { checkIdiom } = require('../services/fetch_idiom');
-
 const router = express.Router();
 
 /** 映射鍵值 */
