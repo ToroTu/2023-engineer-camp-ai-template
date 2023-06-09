5fbef7a16be6f0a80ee558696ae709d26f69819e
ToroTu <tu8247@gmail.com>
2023-06-09 13:05:57 +0800

    test.

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ const { OPENAI_CONTENT } = require('../configs');
 
 const chatAI = require('../services/openai');
 
-const convertToSimp = require('../services/transform_trad2simp');
-
-const { checkIdiom } = require('../services/fetch_idiom');
-
 const router = express.Router();
 
 /** 映射鍵值 */
@@ -27,74 +23,24 @@ const MAP_FLASH_KEY = {
   IDIOM_MORE: 'idiom_more_href',
 };
 
-router
-  .get('/', async (req, res) => {
-    const type = req.flash(MAP_FLASH_KEY.TYPE)[0] || '';
-    const data = {
-      type,
-      [MAP_FLASH_KEY.QUES_CONTENT]:
-        req.flash(MAP_FLASH_KEY.QUES_CONTENT)[0] || '',
-    };
-    const idiomName = req.flash(MAP_FLASH_KEY.IDIOM_NAME)[0];
-    const idiomInterpretation = req.flash(
-      MAP_FLASH_KEY.IDIOM_INTERPRETATION
-    )[0];
-    if (1 == type) {
-      data.idiom_name = idiomName;
-      data.idiom_interpretation = idiomInterpretation;
-      data.idiom_name_cn = convertToSimp(idiomName);
-      data.idiom_interpretation_cn = convertToSimp(idiomInterpretation);
-    } else if (2 == type) {
-      data.idiom_name = idiomName;
-      data.idiom_name_cn = convertToSimp(idiomName);
-    }
-    res.render(
-      'index',
-      Object.assign(
-        {
-          title: convertToSimp('不專業成語大師'),
-          ques_placeholder: convertToSimp(
-            '說吧 想問啥成語意思，或者給我描述幫你想成語'
-          ),
-          type,
-        },
-        data
-      )
-    );
-  })
-  .post('/', async (req, res) => {
-    const { description: ques } = req.body;
-    if (!ques) {
-      req.flash(MAP_FLASH_KEY.TYPE, 3);
-      res.redirect('/');
-      return;
-    }
-    const { status, href, description: idiomExplanation } = await checkIdiom(
-      ques
-    );
-    // 取得成語釋義
-    if (status) {
-      req.flash(MAP_FLASH_KEY.TYPE, 1);
-      req.flash(MAP_FLASH_KEY.IDIOM_NAME, ques);
-      req.flash(MAP_FLASH_KEY.IDIOM_INTERPRETATION, idiomExplanation);
-      req.flash(MAP_FLASH_KEY.IDIOM_MORE, href);
-    } else {
-      const description = `${ques}`;
-      const answer = await chatAI({
-        content: OPENAI_CONTENT,
-        description,
-      });
-      req.flash(MAP_FLASH_KEY.TYPE, 2);
-      req.flash(MAP_FLASH_KEY.IDIOM_NAME, answer);
-      req.flash(MAP_FLASH_KEY.QUES_CONTENT, ques);
-    }
-
-    res.redirect('/');
-  })
-  .post('/api/simp', async (req, res) => {
-    const { content } = req.body;
-    const result = convertToSimp(content);
-    res.json({ cn: result });
-  });
+router.get('/', async (req, res) => {
+  const type = req.flash(MAP_FLASH_KEY.TYPE)[0] || '';
+  const data = {
+    type,
+    [MAP_FLASH_KEY.QUES_CONTENT]:
+      req.flash(MAP_FLASH_KEY.QUES_CONTENT)[0] || '',
+  };
+  res.render(
+    'index',
+    Object.assign(
+      {
+        title: '不專業成語大師',
+        ques_placeholder: '說吧 想問啥成語意思，或者給我描述幫你想成語',
+        type,
+      },
+      data
+    )
+  );
+});
 
 module.exports = router;
